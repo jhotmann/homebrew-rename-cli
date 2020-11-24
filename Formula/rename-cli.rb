c72a712c25c893289bde91cb751de7fe293cc04d
Jordan Hotmann <jhot@protonmail.com>
2020-11-24 13:24:04 -0700

    Update to v7.0.2

--- Formula/rename-cli.rb
+++ Formula/rename-cli.rb
@@ -3,9 +3,9 @@ require "language/node"
 class RenameCli < Formula
   desc "A command line utility for renaming files"
   homepage "https://github.com/jhotmann/node-rename-cli#readme"
-  url "https://registry.npmjs.org/rename-cli/-/rename-cli-7.0.1.tgz"
-  version "7.0.1"
-  sha256 "8711b3130da9ce9cb59c98115d46b9e7a1ec189860fb09f4c145c607b7337cbc"
+  url "https://registry.npmjs.org/rename-cli/-/rename-cli-7.0.2.tgz"
+  version "7.0.2"
+  sha256 "9ff0d3909ae1d91f6b6ad260075c47194197f7b24268e063f5cc52e836b44d66"
 
   depends_on "node"
 
